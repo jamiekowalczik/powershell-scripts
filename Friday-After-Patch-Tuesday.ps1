bfd4ad4c0af140003392a79f0498fc62862742da
jamiekowalczik <jamie.kowalczik@gmail.com>
2015-09-25 16:21:11 -0400

    Update Friday-After-Patch-Tuesday.ps1

--- Friday-After-Patch-Tuesday.ps1
+++ Friday-After-Patch-Tuesday.ps1
@@ -4,7 +4,9 @@
 # Powershell Script to reboot servers to apply patches. 		      #
 # Author: James Kowalczik                                                     # 
 # Creation Date: 04.24.2015                                                   # 
-# Version: 1.0.0                                                              # 
+# Version: 1.0.1							      #
+# Modified: 05.29.2015							      #
+#   -Add functionality to work with not domain computers.                     #  
 #                                                                             # 
 #=============================================================================# 
 
@@ -14,81 +16,60 @@ Param(
    [String]$InstalledAfterDate = "",
    [String]$CSVFile = "",
    [Bool]$Restart = $false,
-   [String]$EmailTo = "alert@me.here",
-   [String]$EmailFrom = "Friday-After-Patch-Tuesday@me.here",
+   [String]$Username = "",
+   [String]$Password = "",
+   [String]$EmailTo = "me@mail.com",
+   [String]$EmailFrom = "Friday-After-Patch-Tuesday@mail.com",
    [String]$EmailSubject = "Friday After Patch Tuesday Maintenance",
-   [String]$EmailServer = "mail.me.here",
-   [Bool]$SendEmail = $false,
+   [String]$EmailServer = "mail.server.com",
+   [Bool]$SendEmail = $true,
    [Bool]$Debug = $true
 )
 
-Function Get-Uptime {
-   $os = Get-WmiObject win32_operatingsystem -ComputerName $Hostname -ErrorAction SilentlyContinue
-   If($os -eq $null){
-      $Display = "Can't connect to: $Hostname"
-      If($Debug){ Write-Host $Display}
-      Return $Display
-   }Else{
-      $uptime = (Get-Date) - ($os.ConvertToDateTime($os.lastbootuptime))
-      $Now = Get-Date
-      $Display = "Server: " + $Hostname + " - Current Time: " + $Now + " - Uptime: " + $Uptime.Days + " days, " + $Uptime.Hours + " hours, " + $Uptime.Minutes + " minutes" 
-      If($Debug){ Write-Host $Display }
-      Return $Display
-   }
-}
-
-Function Get-Recent-Hotfixes {
-   $Display = Get-HotFix -Computername $Hostname | Where {$_.InstalledOn -gt $InstalledAfterDate} | Out-String
-   If($Debug){ Write-Host $Display }
-   Return $Display
-}
-
-Function Reboot-Windows() {
-   If($Restart){
-      $Now = Get-Date
-      If($Wait){
-         $Display = "Rebooting and waiting for $Hostname at $Now"
-         If($Debug){ Write-Host $Display }
-         #Restart-Computer -Force -ComputerName $Hostname -Wait -For WIM
-         Return $Display
-      }Else{
-         $Display = "Rebooting and NOT waiting for $Hostname at $Now"
-         If($Debug){ Write-Host $Display }
-         #Restart-Computer -Force -ComputerName $Hostname
-         Return $Display
-      }
-   }
-}
+Import-Module .\Modules\Uptime.psm1
+Import-Module .\Modules\Hotfixes.psm1
+Import-Module .\Modules\Reboot.psm1
 
 Function DoIt(){
    ## Get Uptime
-   $Output += Get-Uptime | Out-String
+   $Output += Get-Uptime -Hostname $Hostname -Username $Username -Password $Password -DebugMe $Debug | Out-String
 
    ## Get Recently Installed Updates
-   $Output += Get-Recent-Hotfixes | Out-String
+   $Output += Get-RecentHotfixes -Hostname $Hostname -Username $Username -Password $Password -InstalledAfterDate $InstalledAfterDate -DebugMe $Debug | Out-String
 
    ## Reboot Machine
-   $Output += Reboot-Windows | Out-String
-   
+   If($Restart){
+      $Output += Restart-Machine -Hostname $Hostname -Username $Username -Password $Password -Wait $Wait -DebugMe $Debug | Out-String
+   }
+
    Return $Output
 }
 
 $Output = ""
 If($CSVFile -eq ""){
-   ### Run this if no CSV File is used.  This will check 1 device #####
+   ### Run this if no CSV File is used.  This will perform uptime/updates/reboot on 1 device #####
    If($InstalledAfterDAte -eq ""){ $InstalledAfterDate = [DateTime]::Today.AddDays(-1)}
    $Output = DoIt
 }Else{
-   ### Run this is a CSV File is used.  This will check
-   ### all devices in the column with a header of 'Servers'.
+   ### Run this is a CSV File is used.  This will perform
+   ### uptime/updates/reboot on all devices in the column
+   ### with a header of 'Servers'.
    Import-CSV $CSVFile | ForEach-Object {
-      $Hostname = $_.Servers
-      If($_.Wait -eq "wait"){ $Wait = $true }Else{ $Wait = $false }
-      If($InstalledAfterDAte -eq ""){ $InstalledAfterDate = [DateTime]::Today.AddDays(-1)}
-      $Output += DoIt
+      If($_.Wait -ne "skip"){
+         $Hostname = $_.Servers
+         If($_.Username -ne ""){ $Username = $_.Username; $Password = $_.Password }Else{ $Username = ""; $Password = "" }
+         If($_.Wait -eq "wait"){ $Wait = $true }Else{ $Wait = $false }
+         If($InstalledAfterDAte -eq ""){ $InstalledAfterDate = [DateTime]::Today.AddDays(-14)}
+         $Output += DoIt
+      }Else{
+         $Hostname = $_.Servers
+         $Display = "Skipping $Hostname"
+         If($Debug){Write-Host "$Display`n"}
+         $Output += $Display
+      }
    }
 }
 
-If($SendEmail){
+If($SendEmail -eq $true -And $Output -ne "" -And $EmailTo -ne "" -And $EmailSubject -ne "" -And $EmailServer -ne "" -And $EmailFrom -ne ""){
    Send-MailMessage -To $EmailTo -Subject $EmailSubject -Body $Output -SmtpServer $EmailServer -From $EmailFrom
 }
